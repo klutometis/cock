f0a76cf9f96e34b99c3f58c372a379450de81a55
Peter Danenberg <pcd@roxygen.org>
2012-07-30 23:56:30 -0700

    Symbol prepension

--- cock.scm
+++ cock.scm
@@ -1,5 +1,17 @@
 (module cock
-  ()
+  (prepend-@)
   (import chicken scheme)
+  (use coerce debug)
 
-  (set-read-syntax! #\@ (lambda (in) (read in) (values))))
+  (define (prepend-@ symbol)
+    (string->symbol
+     (string-append "@" (symbol->string symbol))))
+
+  (set-read-syntax! #\@
+    (lambda (in)
+      (let ((expression (read in)))
+        (cond ((pair? expression)
+               (values))
+              ((symbol? expression)
+               (prepend-@ expression))
+              (else expression))))))
