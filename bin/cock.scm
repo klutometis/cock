44db5b50f79ed8c50557535d637615f6a7f07e50
Peter Danenberg <pcd@roxygen.org>
2012-08-07 19:14:56 -0700

    PDF output

--- bin/cock.scm
+++ bin/cock.scm
@@ -1,19 +1,50 @@
 #!/usr/bin/env chicken-scheme
 
-(use alist-lib args cock-parse debug usage)
+(use alist-lib
+     args
+     cock-parse
+     debug
+     files
+     miscmacros
+     posix
+     usage)
 
 (define options
-  (list (args:make-option (l latex) #:none "Output to LaTeX" (set! arg #t))
-        (args:make-option (h ? help) #:none "Help" (set! arg #t))))
+  (make-parameter
+   (list (args:make-option (h ? help) #:none "Help" (set! arg #t))
+         (args:make-option (l latex) #:none "Output to LaTeX" (set! arg #t))
+         (args:make-option (o output) (required: "FILE") "Output to FILE")
+         (args:make-option (p pdf) #:none "Output to PDF [Default]" (set! arg #t)))))
 
 (define usage
   (make-usage
    (lambda (program)
      (format #t "Usage: ~a [OPTIONS]... FILE...~%" program)
-     (print (args:usage options)))))
+     (print (args:usage (options))))))
+
+(define (parse-and-write files)
+  (tex-write-docexprs (apply parse-files files)))
+
+(define (option options option)
+  (alist-ref/default options option #f))
 
 (receive (options files)
-  (args:parse (command-line-arguments) options)
-  (cond ((alist-ref/default options 'help #f) (usage))
-        ((null? files) (usage 1))
-        (else (tex-write-docexprs (apply parse-files files)))))
+  (args:parse (command-line-arguments) (options))
+  ;; Let's abstract these.
+  (let ((help (option options 'help))
+        (latex (option options 'latex))
+        (output (option options 'output)))
+    (cond (help (usage))
+          ((null? files) (usage 1))
+          (latex
+           (if output
+               (with-output-to-file output
+                 (lambda () (parse-and-write files)))
+               (parse-and-write files)))
+          (else
+           (let ((directory (create-temporary-directory)))
+             (dotimes (pass 3)
+               (with-output-to-pipe
+                (format "xelatex -shell-escape -output-directory=~a" directory)
+                (lambda () (parse-and-write files))))
+             (when output (file-copy (make-pathname directory "texput.pdf") output #t)))))))
