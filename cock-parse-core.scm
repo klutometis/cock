b996969d01935a0c6d286510020015e2099a3bc9
Peter Danenberg <pcd@roxygen.org>
2012-08-13 14:09:41 -0700

    Add define-record-and-printer.

--- cock-parse-core.scm
+++ cock-parse-core.scm
@@ -4,6 +4,23 @@
 
 (define docexprs (make-parameter (make-stack)))
 
+(define-syntax define-record-and-printer
+  (lambda (expression rename compare)
+    (match expression
+      ((_ record . fields)
+       (let ((%define-record (rename 'define-record))
+             (%define-record-printer (rename 'define-record-printer))
+             (%begin (rename 'begin))
+             (%lambda (rename 'lambda))
+             (%write (rename 'write))
+             (%record->vector (rename 'record->vector)))
+         `(,%begin
+           (,%define-record ,record ,@fields)
+           (,%define-record-printer
+            ,record
+            (,%lambda (record out)
+                 (,%write (,%record->vector record) out)))))))))
+
 (define-record-and-printer null-expression)
 (define null-expression (make-null-expression))
 
