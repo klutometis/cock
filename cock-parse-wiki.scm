05500731549a96be1923820e8a0ddedc98a10d11
Peter Danenberg <pcd@roxygen.org>
2012-08-16 16:29:48 -0700

    Inchoate wiki, con't.

--- cock-parse-wiki.scm
+++ cock-parse-wiki.scm
@@ -1,21 +1,159 @@
 (define (wiki-title title)
   #<#EOF
-  == #{title}
+== #{title}
+
 EOF
 )
+
+(define (wiki-subtitle title)
+  #<#EOF
+=== #{title}
+
+EOF
+)
+
+(define (wiki-subsubtitle title)
+  #<#EOF
+==== #{title}
+
+EOF
+)
+
+(define (wiki-subsubsubtitle title)
+  #<#EOF
+===== #{title}
+
+EOF
+)
+
+(define (wiki-source source)
+  #<#EOF
+<enscript highlight="scheme">#{source}</enscript>
+
+EOF
+)
+
+(define (wiki-procedure signature to)
+  #<#EOF
+<procedure>#{signature} → #{to}</procedure>
+
+EOF
+)
+
+;;; What happens with colons in the definition?
+(define (wiki-parameter parameter definition)
+  #<#EOF
+; #{parameter} : #{definition}
+EOF
+)
+
+(define (wiki-monospace text)
+  #<#EOF
+{{#{text}}}
+EOF
+)
+
+;;; Needs to be generalized.
+(define (wiki-parse-directive doc expr data document)
+  (let ((directive (car doc))
+        (arguments (cdr doc))
+        (data (document-data document)))
+    (case directive
+      ((title)
+       (let ((title (car arguments)))
+         (lambda () (display (wiki-title title)))))
+      ((heading)
+       (let ((title (car arguments)))
+         (lambda ()
+           (hash-table-set! data 'heading-level 1)
+           (display (wiki-subtitle title)))))
+      ((subheading)
+       (let ((title (car arguments)))
+         (lambda ()
+           (hash-table-set! data 'heading-level 2)
+           (display (wiki-subsubtitle title)))))
+      ;; Shit: we're supporting a different language than LaTeX; TODO:
+      ;; intermediate S-expressions over pre-post-order!
+      ((subsubheading)
+       (let ((title (car arguments)))
+         (lambda ()
+           (hash-table-set! data 'heading-level 3)
+           (display (wiki-subsubsubtitle title)))))
+      (else
+       (lambda () (warning "wiki-parse-directive -- Unknown directive" directive))))))
+
+(define (wiki-make-heading heading-level)
+  (match heading-level
+    (0 wiki-title)
+    (1 wiki-subtitle)
+    (2 wiki-subsubtitle)
+    (3 wiki-subsubsubtitle)))
+
+(define wiki-write-source? (make-parameter #t))
+
+(define (wiki-make-description descriptions)
+  (string-join descriptions "\n\n"))
+
+(define (write-wiki-block doc
+                          expr
+                          data
+                          name
+                          item
+                          . rest-items)
+  (let ((heading
+         (wiki-make-heading
+          (hash-table-ref/default
+           data
+           'heading-level
+           0)))
+        (description (wiki-make-description (doc-descriptions doc))))
+    (display (heading (wiki-monospace name)))
+    (string-join (cons item (cons description rest-items)) "\n")
+    (when (wiki-write-source?)
+      (display (wiki-source (with-output-to-string (lambda () (pp expr))))))))
+
+;;; Generalize this.
+(define (make-wiki-procedure template name formals to)
+  (template (cons name formals) (string-join to ", ")))
+
+(define (make-wiki-parameters parameters)
+  (let ((parameters
+         (map
+          (match-lambda ((parameter definition)
+                    (wiki-parameter parameter definition)))
+          parameters)))
+    (string-join parameters "\n")))
+
+(define (wiki-parse-procedure doc expr data name formals)
+  (receive (normal-parameters special-parameters)
+    (doc-normal-and-special-parameters doc)
+    (let ((to (tex-procedure-to special-parameters)))
+      (let ((procedure
+             (make-wiki-procedure wiki-procedure name formals to))
+            (parameters
+             (make-wiki-parameters normal-parameters)))
+        (lambda ()
+          (write-wiki-block doc
+                            expr
+                            data
+                            name
+                            procedure
+                            parameters))))))
 
 (define (wiki-parse-docexpr document docexpr)
   (parameterize ((parse-directive wiki-parse-directive)
                  (parse-procedure wiki-parse-procedure)
-                 (parse-case-lambda wiki-parse-case-lambda)
-                 (parse-parameter wiki-parse-parameter)
-                 (parse-scalar wiki-parse-scalar)
-                 (parse-syntax wiki-parse-syntax)
-                 (parse-read wiki-parse-read)
-                 (parse-record wiki-parse-record)
-                 (parse-module wiki-parse-module))
+                 ;; (parse-case-lambda wiki-parse-case-lambda)
+                 ;; (parse-parameter wiki-parse-parameter)
+                 ;; (parse-scalar wiki-parse-scalar)
+                 ;; (parse-syntax wiki-parse-syntax)
+                 ;; (parse-read wiki-parse-read)
+                 ;; (parse-record wiki-parse-record)
+                 ;; (parse-module wiki-parse-module)
+                 )
     (parse-docexpr document docexpr)))
 
+;;; Needs to be generalized.
 (define (wiki-parse-docexprs document docexprs)
   (let ((parsed-docexprs (make-stack)))
     (stack-for-each
@@ -25,22 +163,10 @@ EOF
                     (wiki-parse-docexpr document docexpr))))
     parsed-docexprs))
 
+;;; Needs to be generalized.
 (define (wiki-write-docexprs docexprs)
   @("Write the source-derived docexprs as svnwiki."
     (docexprs "The parsed docexprs"))
   (let* ((document (make-document (make-hash-table) (make-stack)))
          (parsed-docexprs (wiki-parse-docexprs document docexprs)))
-    (let ((data (document-data document)))
-      (write-template
-       wiki-preamble
-       `((author . ,(hash-table-ref/default data
-                                            'author
-                                            "Anonymous"))
-         (email . ,(hash-table-ref/default data
-                                           'email
-                                           "anonymous@example.org"))
-         (title . ,(hash-table-ref/default data
-                                           'title
-                                           "Documentation")))))
-    (stack-for-each parsed-docexprs (lambda (docexpr) (docexpr)))
-    (display wiki-footer)))
+    (stack-for-each parsed-docexprs (lambda (docexpr) (docexpr)))))
