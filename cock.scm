8d25d45256005f1c7405f662a7acc989a0e944e1
Peter Danenberg <danenberg@google.com>
2014-11-30 20:08:00 -0800

    Drop the setup-helper, it's cleaner.

--- cock.scm
+++ cock.scm
@@ -1,7 +1,15 @@
 (module cock
-  (at
-   prepend-@)
+  (at run-cock)
   (import chicken scheme)
+
+  (define-syntax run-cock
+    (ir-macro-transformer
+     (lambda (expression rename inject)
+       (let ((invocation (cdr expression)))
+         `(handle-exceptions exn
+            (warning (format "Documentation not generated: ~a"
+                             ((condition-property-accessor 'exn 'message) exn)))
+            (run (cock ,@invocation)))))))
 
   ;; This is a hack to provide unescaped @s.
   (define at '@)
--- setup-helper-cock.scm
+++
@@ -1,8 +0,0 @@
-(define-syntax run-cock
-  (ir-macro-transformer
-   (lambda (expression rename inject)
-     (let ((invocation (cdr expression)))
-       `(handle-exceptions exn
-          (warning (format "Documentation not generated: ~a"
-                           ((condition-property-accessor 'exn 'message) exn)))
-          (run (cock ,@invocation)))))))
