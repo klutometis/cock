4f76c786bbba96fa81e764f079640e602bfcf46d
Peter Danenberg <danenberg@google.com>
2014-12-02 12:05:29 -0800

    Don't instantiate the invocation.

--- cock.scm
+++ cock.scm
@@ -5,11 +5,10 @@
   (define-syntax run-cock
     (ir-macro-transformer
      (lambda (expression rename inject)
-       (let ((invocation (cdr expression)))
-         `(handle-exceptions exn
-            (warning (format "Documentation not generated: ~a"
-                             ((condition-property-accessor 'exn 'message) exn)))
-            (run (cock ,@invocation)))))))
+       `(handle-exceptions exn
+          (warning (format "Documentation not generated: ~a"
+                           ((condition-property-accessor 'exn 'message) exn)))
+          (run (cock ,@(cdr expression)))))))
 
   ;; This is a hack to provide unescaped @s.
   (define at '@)
